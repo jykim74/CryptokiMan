4f9badd8df2cc75746760ae96874d452b4655edb
jykim <jykim@ranix.co.kr>
2023-08-25 16:24:26 +0900

    OS 별 qss 지원

--- CryptokiMan/main.cpp
+++ CryptokiMan/main.cpp
@@ -9,6 +9,30 @@
 #include "i18n_helper.h"
 #include "settings_mgr.h"
 
+void setQss( QApplication* app )
+{
+    QString strStyle;
+    QFile qss(":/cryptokiman.qss");
+    qss.open( QFile::ReadOnly );
+
+    strStyle = qss.readAll();
+
+#if defined( Q_OS_WIN32 )
+    QFile css( ":/qt-win.css" );
+#elif defined( Q_OS_MAC)
+    QFile css( ":/qt-mac.css" );
+#endif
+    css.open( QFile::ReadOnly );
+
+    if( css.size() > 0 )
+    {
+        strStyle += "\n";
+        strStyle += css.readAll();
+    }
+
+    app->setStyleSheet( strStyle );
+}
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
@@ -17,9 +41,13 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationDomain( "jssoft.com" );
     QCoreApplication::setApplicationName( "CryptokiMan" );
 
+#if 0
     QFile qss(":/cryptokiman.qss");
     qss.open( QFile::ReadOnly );
     app.setStyleSheet(qss.readAll());
+#else
+    setQss( &app );
+#endif
 
     QCommandLineParser parser;
     parser.setApplicationDescription( QCoreApplication::applicationName() );
