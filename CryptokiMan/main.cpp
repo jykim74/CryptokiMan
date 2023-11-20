2f26727cd1b2daca2d32c2d71c4223d21a5549c6
jykim <jykim@ranix.co.kr>
2023-11-20 15:49:51 +0900

    Linux icon support

--- CryptokiMan/main.cpp
+++ CryptokiMan/main.cpp
@@ -18,6 +18,8 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationDomain( "jssoft.com" );
     QCoreApplication::setApplicationName( "CryptokiMan" );
 
+    QGuiApplication::setWindowIcon(QIcon(":/images/cryptokiman.png"));
+
     QCommandLineParser parser;
     parser.setApplicationDescription( QCoreApplication::applicationName() );
     parser.addHelpOption();
