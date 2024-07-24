986bcd79e0d9126b96989ffdd29be068244b0c80
jykim <jykim@ranix.co.kr>
2024-07-24 11:17:28 +0900

    UI works

--- CryptokiMan/close_session_dlg.cpp
+++ CryptokiMan/close_session_dlg.cpp
@@ -22,7 +22,7 @@ CloseSessionDlg::CloseSessionDlg(QWidget *parent) :
 #if defined(Q_OS_MAC)
     layout()->setSpacing(5);
 #endif
-    resize(width(), minimumSizeHint().height());
+    resize(minimumSizeHint().width(), minimumSizeHint().height());
 }
 
 CloseSessionDlg::~CloseSessionDlg()
