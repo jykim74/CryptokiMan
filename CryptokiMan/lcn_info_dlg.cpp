b04fc6eb2cb7feb0d7560afcc59a268214651f84
jykim <jykim@ranix.co.kr>
2024-11-11 19:03:36 +0900

    소스 개선

--- CryptokiMan/lcn_info_dlg.cpp
+++ CryptokiMan/lcn_info_dlg.cpp
@@ -178,11 +178,12 @@ int LCNInfoDlg::getLCN( const QString& strEmail, const QString& strKey, BIN *pLC
     strURL = getLicenseURI();
     strURL += JS_LCN_PATH;
 
-    QString strBody = QString( "email=%1&key=%2&product=%3&sid=%4&sysinfo=%5")
+    QString strBody = QString( "email=%1&key=%2&product=%3&sid=%4&version=%5&sysinfo=%6")
                           .arg( strEmail.simplified() )
                           .arg( strKey.simplified() )
                           .arg(strProduct.simplified())
                           .arg( SID_.simplified() )
+                          .arg( strVersion )
                           .arg(strInfo.simplified());
 #ifdef QT_DEBUG
     manApplet->log( QString( "Body: %1" ).arg( strBody ));
